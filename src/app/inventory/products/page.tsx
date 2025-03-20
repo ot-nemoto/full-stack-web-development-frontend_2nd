e6a277c58f1ea837f03474c4f8694f5652c7dd24
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-03-20 09:45:17 +0000

    ヘッダー、フッターを追加しよう

--- src/app/inventory/products/page.tsx
+++ src/app/inventory/products/page.tsx
@@ -31,28 +31,36 @@ export default function Page() {
   ];
 
   return (
-    <>
-      <h2 className="text-2xl font-bold mb-4">商品一覧</h2>
-      <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr className="bg-gray-200 text-black">
-            <th className="py-2 px-4 border-b">商品ID</th>
-            <th className="py-2 px-4 border-b">商品名</th>
-            <th className="py-2 px-4 border-b">単価</th>
-            <th className="py-2 px-4 border-b">説明</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id} className="hover:bg-gray-50">
-              <td className="py-2 px-4 border-b">{product.id}</td>
-              <td className="py-2 px-4 border-b">{product.name}</td>
-              <td className="py-2 px-4 border-b">{product.price}</td>
-              <td className="py-2 px-4 border-b">{product.description}</td>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-blue-500 text-white text-lg font-bold py-4 px-6 flex justify-between items-center">
+        在庫管理システム
+      </header>
+      <main className="flex-grow p-4">
+        <h2 className="text-2xl font-bold mb-4">商品一覧</h2>
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead>
+            <tr className="bg-gray-200 text-black">
+              <th className="py-2 px-4 border-b">商品ID</th>
+              <th className="py-2 px-4 border-b">商品名</th>
+              <th className="py-2 px-4 border-b">単価</th>
+              <th className="py-2 px-4 border-b">説明</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id} className="hover:bg-gray-50">
+                <td className="py-2 px-4 border-b">{product.id}</td>
+                <td className="py-2 px-4 border-b">{product.name}</td>
+                <td className="py-2 px-4 border-b">{product.price}</td>
+                <td className="py-2 px-4 border-b">{product.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+      <footer className="bg-blue-500 text-white text-sm py-4 text-center mt-auto">
+        &copy; 2025 full stack web development
+      </footer>
+    </div>
   );
 }
